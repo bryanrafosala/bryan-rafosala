60c6da7926906304c602ec0f08be6f2d57495cf7
Bryan Rafosala <bryanrafosala@bryans-laptop.home>
2024-10-09 13:57:49 +0100

    updated email form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
         </div>
 
         <form
-          action="https://getform.io/f/bwngmzya"
+          action="https://getform.io/f/avregvja"
           method="POST"
           className="xl:pl-10 2xl:pl-20"
         >
